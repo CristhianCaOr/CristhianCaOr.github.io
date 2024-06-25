dc208e3100554a1a40af94fb685677959ede285a
Cristhian <Cristiancanizarezortiz@gmail.com>
2024-06-25 14:42:43 -0500

    creo un archivo js para cada una de las partes de la pagina

---
+++ cambioIdioma/habilidades.js
@@ -0,0 +1,16 @@
+const castellano = document.getElementById('opcionEs');
+const ingles = document.getElementById('opcionEn');
+
+const tituloInicio = document.getElementById('habilidades');
+const tituloAcercaDeMi = document.getElementById('acercaDeMi');
+const tituloHabilidades = document.getElementById('inicio');
+
+
+let tituloInicioOG = tituloInicio.textContent
+let tituloAcercaDeMiOG = tituloAcercaDeMi.textContent
+let tituloHabilidadesOG = tituloHabilidades.textContent
+
+const parrafoHabilidades = document.getElementById('parrafo-habilidades')
+const advertenciaParrafo = document.getElementById('advertencia')
+const habilidadesProgramacion = document.getElementById('tech')
+const misIdiomas = document.getElementById('idiomasQueHablo')
--- cambioIdioma/index.js
+++ cambioIdioma/index.js
@@ -4,45 +4,17 @@ const castellano = document.getElementById('opcionEs');
 const ingles = document.getElementById('opcionEn');
 
 
-// titulos
-const tituloInicio = document.getElementById('habilidades');
-const tituloAcercaDeMi = document.getElementById('acercaDeMi');
-const tituloHabilidades = document.getElementById('inicio');
-
-
-
-// secciones de la pagina
-const inicio = document.getElementsByClassName('boton-inicio');
-const acercaDeMi = document.getElementsByClassName('boton-acdm');
-const habilidades = document.getElementsByClassName('boton-habilidades');
-
 
 // variables del inicio
 const nombre = document.getElementById('nombre');
 const profesion = document.getElementById('profesion');
 const parrafo1 = document.getElementById('parrafo-inicio');
+
 let nombreOG = nombre.textContent
 let profesionOG = profesion.textContent
 let parrafo1OG = parrafo1.textContent
 
 
-// variables acerca de mi
-
-const portadaAcercaDeMi = document.getElementById('titulo-acdm');
-const parrafoacdm1 = document.getElementById('parrafo-acdm-1');
-const parrafoacdm2 = document.getElementById('parrafo-acdm-2');
-const parrafoacdm3 = document.getElementById('parrafo-acdm-3');
-
-// variables habilidades
-
-const parrafoHabilidades = document.getElementById('parrafo-habilidades')
-const advertenciaParrafo = document.getElementById('advertencia')
-const habilidadesProgramacion = document.getElementById('tech')
-const misIdiomas = document.getElementById('idiomasQueHablo')
-
-// codigo de ingles
-
-function cambioIdiomaInicio(){
     ingles.addEventListener('click', function(event) {
         event.preventDefault();
         nombre.textContent = "El mother fucker";
@@ -52,7 +24,6 @@ function cambioIdiomaInicio(){
         event.preventDefault();
         nombre.textContent = nombreOG;
     })
-}
 
 
 
@@ -60,6 +31,5 @@ function cambioIdiomaInicio(){
 
 
 
-cambioIdiomaInicio()
 
 

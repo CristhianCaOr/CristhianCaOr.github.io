0549cb29508f4997bb3510b8e79795488b604038
Cristhian <Cristiancanizarezortiz@gmail.com>
2024-06-25 13:38:11 -0500

    ya dedscubri el error, ahora a empezar a programar de verdad

--- cambioIdioma/index.js
+++ cambioIdioma/index.js
@@ -1,34 +1,33 @@
 
 // idioma
-const castellano = getElementsByClassName('opcionEs')
-const ingles = getElementsByClassName('opcionEn')
+const castellano = document.getElementById('opcionEs');
+const ingles = document.getElementById('opcionEn');
 
 
 // titulos
-const tituloInicio = getElementById('habilidades')
-const tituloAcercaDeMi = getElementById('acercaDeMi') 
-const tituloHabilidades = getElementById('inicio')
+const tituloInicio = document.getElementById('habilidades');
+const tituloAcercaDeMi = document.getElementById('acercaDeMi');
+const tituloHabilidades = document.getElementById('inicio');
 
 
 
 // secciones de la pagina
-const inicio = getElementsByClassName('boton-inicio')
-const acercaDeMi = getElementsByClassName('boton-acdm')
-const habilidades = getElementsByClassName('boton-habilidades')
-
+const inicio = document.getElementsByClassName('boton-inicio');
+const acercaDeMi = document.getElementsByClassName('boton-acdm');
+const habilidades = document.getElementsByClassName('boton-habilidades');
 
 
 // variables del inicio
-const nombre = document.getElementById('nombre')
-const profesion = document.getElementById('profesion')
-const parrafo1 = document.getElementById('parrafo-inicio')
+const nombre = document.getElementById('nombre');
+const profesion = document.getElementById('profesion');
+const parrafo1 = document.getElementById('parrafo-inicio');
 
 // variables acerca de mi
 
-const portadaAcercaDeMi = document.getElementById('titulo-acdm')
-const parrafoacdm1 = document.getElementById('parrafo-acdm-1')
-const parrafoacdm2 = document.getElementById('parrafo-acdm-2')
-const parrafoacdm3 = document.getElementById('parrafo-acdm-3')
+const portadaAcercaDeMi = document.getElementById('titulo-acdm');
+const parrafoacdm1 = document.getElementById('parrafo-acdm-1');
+const parrafoacdm2 = document.getElementById('parrafo-acdm-2');
+const parrafoacdm3 = document.getElementById('parrafo-acdm-3');
 
 // variables habilidades
 
@@ -39,7 +38,10 @@ const misIdiomas = document.getElementById('idiomasQueHablo')
 
 // codigo de ingles
 
-ingles.addEventListener('click'), () => {
-    nombre.textContent = "Your mother"
+ingles.addEventListener('click', function(event) {
+    event.preventDefault();
+    nombre.textContent = "El mother fucker";
+})
 
-}
+
+

5e896076de8043b98938eed67dbc3cc4a441f8ba
Cristhian <Cristiancanizarezortiz@gmail.com>
2024-06-25 11:35:49 -0500

    agregue los nombres de los ids a todos los elementos que voy a utilizar en el js y ademas cambie el nav para que todas tengan lo mismo

---
+++ cambioIdioma/index..js
@@ -0,0 +1,31 @@
+const idiomas = document.getElementsByClassName('opcion')
+
+// titulos
+const tituloInicio = getElementById('habilidades')
+const tituloAcercaDeMi = getElementById('acercaDeMi') 
+const tituloHabilidades = getElementById('inicio')
+
+
+// secciones de la pagina
+const inicio = getElementsByClassName('boton-inicio')
+const acercaDeMi = getElementsByClassName('boton-acdm')
+const habilidades = getElementsByClassName('boton-habilidades')
+
+
+
+// variables del inicio
+const nombre = document.getElementById('nombre')
+const profesion = document.getElementById('profesion')
+const parrafo1 = document.getElementById('parrafo-inicio')
+
+// variables acerca de mi
+
+const portadaAcercaDeMi = document.getElementById('titulo-acdm')
+const parrafoacdm1 = document.getElementById('parrafo-acdm-1')
+const parrafoacdm2 = document.getElementById('parrafo-acdm-2')
+const parrafoacdm3 = document.getElementById('parrafo-acdm-3')
+
+// variables habilidades
+
+
+

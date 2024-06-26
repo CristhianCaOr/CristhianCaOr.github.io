c9d7ce539a7a1f0e5c41e82999a1b24f1f3bbde3
Cristhian <Cristiancanizarezortiz@gmail.com>
2024-06-26 09:01:49 -0500

    el JS me vencio, asi que decidi hacer todo en HTML. Lo siento madre, te desepcione

--- cambioIdioma/secciones.js
+++
@@ -1,8 +0,0 @@
-// idioma
-const castellano = document.getElementById('opcionEs');
-const ingles = document.getElementById('opcionEn');
-
-
-const inicio = document.getElementsByClassName('boton-inicio');
-const acercaDeMi = document.getElementsByClassName('boton-acdm');
-const habilidades = document.getElementsByClassName('boton-habilidades');
